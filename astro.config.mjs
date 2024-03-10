4672012e2c0b64b436119f64d7af897aae3b9c5f
Gabriel Golveira <gabriel.golveira@gmail.com>
2024-03-10 16:36:21 -0300

    New layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://regal-speculoos-6e0875.netlify.app/",
+  integrations: [preact(), tailwind(), alpinejs()],
 });
